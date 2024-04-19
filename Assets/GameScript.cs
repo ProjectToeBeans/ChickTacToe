6204fdffce79c391f52b151349697f167e496769
Shish <shish@shishnet.org>
2024-04-19 10:07:49 +0100

    layout updates

--- Assets/GameScript.cs
+++ Assets/GameScript.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class GameScript : MonoBehaviour
 {
+    public int w = 3;
+    public int h = 3;
+
+    public void OnSquareClicked(int x)
+    {
+        Debug.Log("Clicked " + x + "x");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
